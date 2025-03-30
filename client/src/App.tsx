fd8e9abe9f69955a973778b20985734e08ccf380
riquelme <riquelme3m@outlook.com>
2025-03-30 16:48:19 -0300

    feat/fe/Registration page + successfull registration page done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import {Login} from "../src/pages/Login/index";
 import Information  from "../src/pages/Information/index";
 import Dashboard from "../src/pages/Dashboard/index";
 import Registration from "./pages/Registration";
+import Success from "./pages/Success";
+
 function App() {
   return (
     
@@ -11,6 +13,7 @@ function App() {
         <Route path="/information" element={<Information/>} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/registration" element={<Registration/>}/>
+        <Route path="/success" element={<Success/>}/>
 
       </Routes>
     
