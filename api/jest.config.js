f92850051d317a3edc8205c1698f95936f108d5b
André Luiz Alves <andrelac963@gmail.com>
2025-04-01 20:34:46 -0300

    feat: Finalizing unit tests for user

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,12 +1,23 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  rootDir: "./src",
+  testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+  moduleFileExtensions: ["ts", "js"],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    "^.+\\.ts$": "ts-jest",
   },
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: "../coverage",
+  coverageReporters: ["text", "lcov"],
+  collectCoverageFrom: [
+    "**/*.{ts,js}",
+    "!**/node_modules/**",
+    "!**/dist/**",
+    "!**/*.d.ts",
+    "!app.ts",
+  ],
+  coveragePathIgnorePatterns: ["src/config/", "src/routes/"],
+  testPathIgnorePatterns: ["src/config/", "src/routes/"],
 };
